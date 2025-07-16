7f14138b39235fd248c9b8e4282f8e28a6efa6b8
Alexandra Ciotau <ciotauaa@gmail.com>
2025-07-16 14:56:17 +0100

    filtration

--- src/left_int/intersection.py
+++ src/left_int/intersection.py
@@ -6,9 +6,13 @@ print("This program prints the intersection of (B_+)y_n with (B_+)y_m!")
 
 def check_input(n,m):
     if n >= m:
-        raise ValueError(f"n={n} must be less than m={m}")
+        raise ValueError(f"n={n} must be less than m={m}!")
     if n < 4:
-        raise ValueError(f"n={n} must be at least 4")
+        raise ValueError(f"n={n} must be at least 4!")
+
+def check_filtr(w):
+    if w < 9:
+        raise ValueError(f"The filtration must be greater or equal to 9!")
 
 
 def valid_pairs(x):
@@ -23,12 +27,14 @@ def valid_pairs(x):
 
 n=int(input("Enter your first weight: "))
 m=int(input("Enter your second weight: "))
+w=int(input("Enter the desired filtration:"))
 check_input(n,m)
+check_filtr(w)
 a, b = symbols("a b")
 terms = []
-pairs = valid_pairs(n)
+pairs = valid_pairs(w-m)
 for i, j in pairs:
-    term = f"{{(a**{i}* (a+b)**{j}) b (1-b)y_{n+m}}}"
+    term = f"{{(a**{i}* (a+b)**{j}) b (1-b)y_{w}}}"
     terms.append(term)
 print(f"The intersection is span_{{K}}" ,", ".join(terms))
 
